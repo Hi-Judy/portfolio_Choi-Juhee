df94f00a87c39b4f356e2991acb6a7b7c73a8452
hi-judy <bbo3603@gmail.com>
2022-01-13 10:31:37 +0900

    주희 13일 commit

--- src/main/java/egov/mes/manufacture/dao/ManufactureMapper.java
+++ src/main/java/egov/mes/manufacture/dao/ManufactureMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import egovframework.rte.psl.dataaccess.mapper.Mapper;
 
+
 @Mapper("manufactureMapper")
 public interface ManufactureMapper {
 	//미계획조회
@@ -12,4 +13,9 @@ public interface ManufactureMapper {
 	//자재 조회
 	List<ManufacturePlanVO> selectRes(ManufacturePlanVO mPlanVo);
 
+	//생산계획 추가
+	int insertPlan(ManufacturePlanVO palnVO);
+	
+	//생산계획 디테일 추가
+	int insertPlanDetail(ManufacturePlanVO palnVO);
 }
