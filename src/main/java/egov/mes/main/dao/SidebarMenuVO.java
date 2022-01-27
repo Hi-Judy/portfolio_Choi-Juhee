18df8a7c7d8e03d9209df69a6336508a54df2b8d
1234 <tig02389@naver.com>
2022-01-27 17:45:49 +0900

    2201027 1745

---
+++ src/main/java/egov/mes/main/dao/SidebarMenuVO.java
@@ -0,0 +1,15 @@
+package egov.mes.main.dao;
+
+import lombok.Data;
+
+@Data
+public class SidebarMenuVO {
+	String menuNo;
+	String menuOrdr;
+	String menuNm;
+	String upperMenuId;
+	String relateImagePath;
+	String relateImageNm;
+	String chkUrl;
+
+}
