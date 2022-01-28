aa52ba85d71c0826f4cebd8b16086880c155b766
admin <dlsdl1210@naver,com>
2022-01-28 17:44:59 +0900

    0128_최창인_학원에서끝 사원관리 페이지 간단하게 완성 -> 자재관리 시작할것같음

--- src/main/java/egov/mes/Employee/service/EmployeeService.java
+++ src/main/java/egov/mes/Employee/service/EmployeeService.java
@@ -7,5 +7,8 @@ import egov.mes.Employee.dao.EmployeeVO;
 public interface EmployeeService {	
 	
 	List<EmployeeVO> EmpAllFind();	//사워정보 조회
-
+	List<EmployeeVO> IdFind( EmployeeVO empVO );  //사원ID 조건 조회
+	
+	void EmpAddData ( EmployeeVO empVO );		//사원정보 입력
+	void UpdateDatas( List<EmployeeVO> empVO );		//사원정보 수정
 }
--- src/main/java/egov/mes/Employee/service/Impl/EmployeeServiceImpl.java
+++ src/main/java/egov/mes/Employee/service/Impl/EmployeeServiceImpl.java
@@ -14,11 +14,36 @@ import egov.mes.Employee.service.EmployeeService;
 public class EmployeeServiceImpl implements EmployeeService {
 	
 	@Autowired EmployeeMapper mapper;
-
+	
+	//사원정보 전체조회
 	@Override
 	public List<EmployeeVO> EmpAllFind() {
 		
 		return mapper.EmpAllFind();
+	}
+	
+	//사원ID 조건 조회
+	@Override
+	public List<EmployeeVO> IdFind( EmployeeVO empVO ) {
+		
+		return mapper.IdFind(empVO);
+	}
+	
+	//사원정보 입력
+	@Override	
+	public void EmpAddData(EmployeeVO empVO) {
+		mapper.EmpAddData(empVO);
+		
+	}
+	
+	//사원정보 수정
+	@Override
+	public void UpdateDatas(List<EmployeeVO> empVO) {
+		for(EmployeeVO EmpVO : empVO) {
+			System.out.println(EmpVO);
+			mapper.UpdateDatas(EmpVO);
+		}
+		
 	}
 
 }
