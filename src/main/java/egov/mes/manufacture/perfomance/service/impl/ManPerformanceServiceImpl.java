a0bd6e33fa051f56326413502ca528ee43021d2d
hi-judy <bbo3603@gmail.com>
2022-02-08 09:06:41 +0900

    0208AMJUHEE

--- src/main/java/egov/mes/manufacture/perfomance/service/impl/ManPerformanceServiceImpl.java
+++ src/main/java/egov/mes/manufacture/perfomance/service/impl/ManPerformanceServiceImpl.java
@@ -1,9 +1,12 @@
 package egov.mes.manufacture.perfomance.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import egov.mes.manufacture.perfomance.dao.ManPerformanceMapper;
+import egov.mes.manufacture.perfomance.dao.ManPerformanceVO;
 import egov.mes.manufacture.perfomance.service.ManPerformanceService;
 
 @Service("ManPerformanceService")
@@ -11,5 +14,36 @@ public class ManPerformanceServiceImpl implements ManPerformanceService {
 	
 	@Autowired 
 	private ManPerformanceMapper mapper;
+
+	//생산실적관리
+	@Override
+	public List<ManPerformanceVO> selectDef(ManPerformanceVO performanceVO) {
+		System.out.println("11");
+
+		if(performanceVO.getPerformanceSort().equals("podtCode")) {
+			System.out.println("제품별 실적조회: " + mapper.selectPodtDef(performanceVO));
+			return mapper.selectPodtDef(performanceVO);
+		} else if(performanceVO.getPerformanceSort().equals("monthCode") ) {
+			System.out.println("월별 실적조회: " + mapper.selectMonthDef(performanceVO));
+			return mapper.selectMonthDef(performanceVO);
+		}
+		
+		return null;
+	}
+
+
+//	@Override
+//	public ManPerformanceVO selectPodtDef(ManPerformanceVO performanceVO) {
+//		return mapper.selectPodtDef(performanceVO);
+//	}
+//
+//	@Override
+//	public ManPerformanceVO selectMonthDef(ManPerformanceVO performanceVO) {
+//		return mapper.selectMonthDef(performanceVO);
+//	}
+
+	
+
+	
 
 }
